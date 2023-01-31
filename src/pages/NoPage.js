2dae4c9a874bebf1e707e222042915bbba42a047
Hector Acosta <1Hector.AC@gmail.com>
2023-01-31 14:36:23 -0800

    Added NoPage page content.

---
+++ src/pages/NoPage.js
@@ -0,0 +1,11 @@
+const NoPage = () => {
+    return (
+        <div className="text-center whiteText">
+            <br />
+            <h1>No Page was Found</h1>
+            <p>Sorry, We could not find the page requested.</p>
+            <br/>
+        </div>
+    )
+};
+export default NoPage;
