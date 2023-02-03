d294f0c29346534a336e13ab13570a83d16093fa
Hector Acosta <1Hector.AC@gmail.com>
2023-02-03 14:58:08 -0800

    Added navbar in Layout.js.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,31 @@
+import { Outlet, NavLink } from "react-router-dom";
 const Layout = () =>{
     return (
         <>
-            <p>Layout Test</p>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <NavLink className="navbar-brand" to="/">Money Saver Calculator</NavLink>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+                <ul className="navbar-nav me-auto">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/SaleCalculator">SaleCalculator</NavLink>
+                    </li>  
+                </ul>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><NavLink className="nav-link dropdown-item" to="/" >Home</NavLink></li>
+                    <li><NavLink className="nav-link dropdown-item" to="/SaleCalculator" >SaleCalculator</NavLink></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+            <Outlet/>
         </>
     )
 };
