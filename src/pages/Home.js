268bdc1e162767f5d24e1366043aa81105afa4bc
Hector Acosta <1Hector.AC@gmail.com>
2023-02-04 14:27:58 -0800

    Added some content to Home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,38 @@
 const Home = () => {
-    return(
-        <>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-        </>
+    return (
+        <div className="text-center container">
+            <div className="titleSection">
+                <h1>Money Saver Calculator</h1>
+                <p><i>A website to help you calculate how much your are saving/spending.</i></p>
+                <hr />
+            </div>
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="smallPanel">
+                        <h3>Test Title</h3>
+                        <hr />
+                        <p>Description Added here later.</p>
+                    </div>
+
+                </div>
+                <div className="col-md-4">
+                    <div className="smallPanel">
+                        <h3>Test Title</h3>
+                        <hr />
+                        <p>Description Added here later.</p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="smallPanel">
+                        <h3>Test Title</h3>
+                        <hr />
+                        <p>Description Added here later.</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     )
 };
 export default Home;
