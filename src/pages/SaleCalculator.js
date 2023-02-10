559bc467ac4f3cff1724996ffe7eb8eddc8cc2bd
Hector Acosta <1Hector.AC@gmail.com>
2023-02-10 14:43:20 -0800

    Improve validation and display in SaleCalculator

--- src/pages/SaleCalculator.js
+++ src/pages/SaleCalculator.js
@@ -4,37 +4,61 @@ const SaleCalculator = () => {
     const [cost, setCost] = useState(0);
     const [percentage, setPercentage] = useState(0);
     function changeCost(e) {
-        let input = e.target.value;
+        let input = Number(e.target.value);
         if (input > 0 && input <= 1000000)
             setCost(input);
+        else if (input > 1000000) {
+            e.target.value = 1000000;
+            setCost(1000000);
+        }
+        else if (input < 0) {
+            e.target.value = 0;
+            setCost(0);
+        }
+        else
+            setCost(0);
     }
     function changePercentage(e) {
-        let input = e.target.value;
-        if (input > 0 && input < 100)
+        let input = Number(e.target.value);
+        if (input > 0 && input <= 100)
             setPercentage(input);
+        else if (input > 100) {
+            e.target.value = 100;
+            setPercentage(100);
+        }
+        else if (input < 0) {
+            e.target.value = 0;
+            setPercentage(0);
+        }
+        else
+            setPercentage(0);
     }
     return (
         <>
             <div className="text-center container whiteText">
                 <div className="titleSection">
                     <h2>Sale Calculator</h2>
-                    <p><i>Calculate the percentage off of a purchase.</i></p>
+                    <p><i>Calculate the savings of a percentage off sale.</i></p>
                 </div>
                 <hr />
                 <div className="row">
                     <div className="col-sm-3 offset-sm-3 col-6">
                         <h5>Cost of Item</h5>
-                        <input type="number" className="form-control" onChange={changeCost} placeholder="Item Price" maxLength={3} />
+                        <input type="number" className="form-control" onChange={changeCost} placeholder="Item Price" max={"99"} />
                     </div>
                     <div className="col-sm-3 col-6">
                         <h5>Percentage Off</h5>
                         <input type="number" className="form-control" onChange={changePercentage} placeholder="Percentage" />
                     </div>
                 </div>
+                <p>Note: item price and percentage are limited to a million and 100 respectively.</p>
                 <hr />
-                <h5>Cost (before Saving): {cost}</h5>
-                <h5>Saving: {percentage === 0 || cost=== 0?0:(percentage / 100 * cost).toFixed(2)}</h5>
-                <h5>Cost (after Saving): {percentage ===0 || cost===0?0:(cost - percentage / 100 * cost).toFixed(2)}</h5>
+                <h5>New Price</h5>
+                <p style={{lineHeight:'.1'}}><i>(Cost - Saving = New Cost)</i></p>
+                <h5>
+                    {cost.toFixed(2) + " - "}
+                    {(percentage / 100 * cost).toFixed(2) + " = " + (cost - percentage / 100 * cost).toFixed(2)}
+                </h5>
                 <br />
             </div>
         </>
