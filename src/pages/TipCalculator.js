95e4ff6ec04cecba718cbe7661db34377a9e950c
Hector Acosta <1Hector.AC@gmail.com>
2023-02-12 14:42:55 -0800

    Added input validation & better display in TipCalculator.

--- src/pages/TipCalculator.js
+++ src/pages/TipCalculator.js
@@ -2,29 +2,63 @@ import { useState } from "react";
 
 const TipCalculator = () => {
     const [cost, setCost] = useState(0);
-    const [percentage, setPercentage] =  useState(0);
+    const [percentage, setPercentage] = useState(0);
+    function changeCost(e) {
+        let input = Number(e.target.value);
+        if (input > 0 && input <= 1000000)
+            setCost(input);
+        else if (input > 1000000) {
+            e.target.value = 1000000;
+            setCost(1000000);
+        }
+        else if (input < 0) {
+            e.target.value = 0;
+            setCost(0);
+        }
+        else
+            setCost(0);
+    }
+    function changePercentage(e) {
+        let input = Number(e.target.value);
+        if (input > 0 && input <= 100)
+            setPercentage(input);
+        else if (input > 100) {
+            e.target.value = 100;
+            setPercentage(100);
+        }
+        else if (input < 0) {
+            e.target.value = 0;
+            setPercentage(0);
+        }
+        else
+            setPercentage(0);
+    }
     return (
         <>
             <div className="text-center container whiteText">
                 <div className="titleSection">
                     <h2>Tip Calculator</h2>
-                    <p><i>Calculate the extra cost for a tip.</i></p>
+                    <p><i>Calculate the cost of an item with a tip.</i></p>
                 </div>
                 <hr />
-                <br />
                 <div className="row">
-                    <div className="col-md-2 offset-md-3">
-                        <input type="number" className="form-control" onChange={(e)=>setCost(e.target.value)} placeholder="cost"/>
+                    <div className="col-md-3 offset-md-3 col-6">
+                        <h5>Cost of Item</h5>
+                        <input type="number" className="form-control" onChange={changeCost} placeholder="Item price" />
                     </div>
-                    <div className="col-md-2"><h5>with a tip of</h5> </div>
-                    <div className="col-md-2">
-                    <input type="number" className="form-control" onChange={(e)=>setPercentage(e.target.value)} placeholder="percentage"/>
+                    <div className="col-md-3 col-6">
+                        <h5>Tip Percentage</h5>
+                        <input type="number" className="form-control" onChange={changePercentage} placeholder="Percentage" />
                     </div>
                 </div>
-                <hr/>
-                <h5>Cost(before tip): {cost}</h5>
-                <h5>Tip amount: {(cost *(percentage/100)).toFixed(2)}</h5>
-                <h5>Cost(with tip): {(Number(cost) + (cost*(percentage/100))).toFixed(2)} </h5>
+                <p>Note: item price and percentage are limited to a million and 100 respectively.</p>
+                <hr />
+                <h5>New Price</h5>
+                <p style={{ lineHeight: '.1' }}><i>(Cost + Tip = New Cost)</i></p>
+                <h5>
+                    {cost.toFixed(2) + " + "}
+                    {(cost * (percentage / 100)).toFixed(2) + " = " + (cost + (cost * (percentage / 100))).toFixed(2)}
+                </h5>
                 <br />
             </div>
         </>
