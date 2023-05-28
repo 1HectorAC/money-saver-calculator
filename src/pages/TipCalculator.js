1a716b4fb070c6494d819c5f60da7a18bc9afd64
Hector Acosta <1Hector.AC@gmail.com>
2023-05-28 14:05:56 -0700

    Refactored TipCalculator page.

--- src/pages/TipCalculator.js
+++ src/pages/TipCalculator.js
@@ -1,67 +1,54 @@
 import { useState } from "react";
+import TitlePanel from '../components/TitlePanel';
 
 const TipCalculator = () => {
     const [cost, setCost] = useState(0);
     const [percentage, setPercentage] = useState(0);
+
     function changeCost(e) {
         let input = Number(e.target.value);
-        if (input > 0 && input <= 1000000)
-            setCost(input);
-        else if (input > 1000000) {
-            e.target.value = 1000000;
-            setCost(1000000);
-        }
-        else if (input < 0) {
-            e.target.value = 0;
-            setCost(0);
-        }
-        else
-            setCost(0);
+
+        if (input > 1000000)
+            e.target.value = input = 1000000;
+        else if (input < 0)
+            e.target.value = input = 0;
+
+        setCost(input);
     }
+
     function changePercentage(e) {
         let input = Number(e.target.value);
-        if (input > 0 && input <= 100)
-            setPercentage(input);
-        else if (input > 100) {
-            e.target.value = 100;
-            setPercentage(100);
-        }
-        else if (input < 0) {
-            e.target.value = 0;
-            setPercentage(0);
-        }
-        else
-            setPercentage(0);
+
+        if (input > 100)
+            e.target.value = input = 100;
+        else if (input < 0)
+            e.target.value = input = 0;
+
+        setPercentage(input);
     }
     return (
-        <>
-            <div className="text-center container whiteText">
-                <div className="titleSection">
-                    <h2>Tip Calculator</h2>
-                    <p><i>Calculate the cost of an item with a tip.</i></p>
+        <div className="text-center container whiteText">
+            <TitlePanel titleText="Tip Calculator" subText="Calculate the cost of an item with a tip." />
+            <div className="row">
+                <div className="col-md-3 offset-md-3 col-6">
+                    <h5>Cost of Item</h5>
+                    <input type="number" className="form-control" onChange={changeCost} placeholder="Item price" />
                 </div>
-                <hr />
-                <div className="row">
-                    <div className="col-md-3 offset-md-3 col-6">
-                        <h5>Cost of Item</h5>
-                        <input type="number" className="form-control" onChange={changeCost} placeholder="Item price" />
-                    </div>
-                    <div className="col-md-3 col-6">
-                        <h5>Tip Percentage</h5>
-                        <input type="number" className="form-control" onChange={changePercentage} placeholder="Percentage" />
-                    </div>
+                <div className="col-md-3 col-6">
+                    <h5>Tip Percentage</h5>
+                    <input type="number" className="form-control" onChange={changePercentage} placeholder="Percentage" />
                 </div>
-                <p>Note: item price and percentage are limited to a million and 100 respectively.</p>
-                <hr />
-                <h5>New Price</h5>
-                <p style={{ lineHeight: '.1' }}><i>(Cost + Tip = New Cost)</i></p>
-                <h5>
-                    {cost.toFixed(2) + " + "}
-                    {(cost * (percentage / 100)).toFixed(2) + " = " + (cost + (cost * (percentage / 100))).toFixed(2)}
-                </h5>
-                <br />
             </div>
-        </>
+            <p><i>Note: item price and percentage are limited to a million and 100 respectively.</i></p>
+            <hr />
+            <h5>New Price</h5>
+            <p className="italicText">(Cost + Tip = New Cost)</p>
+            <h5>
+                {cost.toFixed(2) + " + "}
+                {(cost * (percentage / 100)).toFixed(2) + " = " + (cost + (cost * (percentage / 100))).toFixed(2)}
+            </h5>
+            <br />
+        </div>
     )
 }
 export default TipCalculator;
